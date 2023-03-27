b9308ce41740a92a7fe00548663879582669a972
voduchau <h002250@cybozu.onmicrosoft.com>
2023-03-27 14:36:23 +0700

    test

--- src/attachment/index.ts
+++ src/attachment/index.ts
@@ -99,6 +99,7 @@ let exportAttachment;
     }
     willUpdate(changedProperties: PropertyValues) {
       console.log('xx')
+      console.log('123123')
       if (changedProperties.has("language")) {
         this._locale = this._getLocale();
       }
