b6271504215139535ef706fc9c0726e778ffbd55
voduchau <h002250@cybozu.onmicrosoft.com>
2023-03-27 14:23:48 +0700

    test

--- src/attachment/style.ts
+++ src/attachment/style.ts
@@ -48,7 +48,7 @@ export const ATTACHMENT_CSS = `
     display: none;
   }
   .kuc-attachment__group__files {
-    border: solid 1px green;
+    border: solid 1px yellow;
     background-color: #eeeeee;
     padding: 16px 4px;
     display: block;
